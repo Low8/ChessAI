017dd93c64c9b7c75cd25353c51db39c329d8e06
Louison Masson <louison.masson@gmailcom>
2025-10-04 13:21:26 +0200

    minMax fonctionnelle plus évaluer fonctionnelle mais à ajuster

--- ai.py
+++ ai.py
@@ -1,4 +1,5 @@
 import random
+import copy
 from board import Board
 
 class Ai:
@@ -43,80 +44,214 @@ class Random_ai:
 
 class MinMax_ai:
     def evaluate(self, color, board):
+        """
+        Évalue la position du plateau pour une couleur donnée
+        Score positif = bon pour 'color', Score négatif = mauvais pour 'color'
+        """
 
-        # Fonction permettant d'évaluer le tableau en fonction d'une couleur
-        # On ajoute les points de la piece si elle est de la couleur voulu sinon on les soustrer
+        opponent_color = "b" if color == "w" else "w"
+
+        # Avant tout un echec renvoie imédiatement le pire score possbile ou le meilleur en cas de victoire
+
+        if board.check_game_status(color) == 'checkmate':
+            return -1000000
+        elif board.check_game_status(color) == 'stalemate':
+            return 0
+        
+        if board.check_game_status(opponent_color) == 'checkmate':
+            return 1000000
+        
+
+
+        # Different tableau selon la valeur et la position des pieces
 
         points = {
-            "p" : 1,
-            "n" : 3,
-            "b" : 3,
-            "r" : 5,
-            "q" : 9,
-            "k" : 100
+            "p": 100,
+            "n": 320,
+            "b": 330,
+            "r": 500,
+            "q": 900,
         }
-
+        
+        # Bonus de position pour les pions (encourage l'avancement)
+        pawn_position_bonus = [
+            [0,  0,  0,  0,  0,  0,  0,  0],  # Ligne 0 (promotion)
+            [50, 50, 50, 50, 50, 50, 50, 50],  # Ligne 1
+            [10, 10, 20, 30, 30, 20, 10, 10],  # Ligne 2
+            [5,  5, 10, 25, 25, 10,  5,  5],   # Ligne 3
+            [0,  0,  0, 20, 20,  0,  0,  0],   # Ligne 4
+            [5, -5,-10,  0,  0,-10, -5,  5],   # Ligne 5
+            [5, 10, 10,-20,-20, 10, 10,  5],   # Ligne 6
+            [0,  0,  0,  0,  0,  0,  0,  0]    # Ligne 7
+        ]
+        
+        # Bonus de position pour les cavaliers (encourage le centre)
+        knight_position_bonus = [
+            [-50,-40,-30,-30,-30,-30,-40,-50],
+            [-40,-20,  0,  0,  0,  0,-20,-40],
+            [-30,  0, 10, 15, 15, 10,  0,-30],
+            [-30,  5, 15, 20, 20, 15,  5,-30],
+            [-30,  0, 15, 20, 20, 15,  0,-30],
+            [-30,  5, 10, 15, 15, 10,  5,-30],
+            [-40,-20,  0,  5,  5,  0,-20,-40],
+            [-50,-40,-30,-30,-30,-30,-40,-50]
+        ]
+        
         score = 0
 
+        # Ajout des bonus de placement des pieces selon les tableau ci-dessus
         for x in range(8):
             for y in range(8):
                 piece = board.grid[x][y].piece
                 if piece:
                     value = points.get(piece.type, 0)
+                    
+                    if piece.type == "p":
+                        if piece.color == "w":
+                            value += pawn_position_bonus[x][y]
+                        else:
+                            value += pawn_position_bonus[7-x][y]
+                    
+                    elif piece.type == "n":
+                        if piece.color == "w":
+                            value += knight_position_bonus[x][y]
+                        else:
+                            value += knight_position_bonus[7-x][y]
+                    
+                    # Ajouter ou soustraire selon la couleur
                     if piece.color == color:
                         score += value
                     else:
                         score -= value
+        
+        # Bonus pour la mobilité (nombre de coups légaux) Une piece avec beaucoup 
+        # de posibilité de déplacement sera plus forte qu'une avec moins
+        my_pieces = board.get_all_playable_pieces(color)
+        my_moves = 0
+        for x, y in my_pieces:
+            moves = board.get_legal_moves((x, y))
+            for move in moves:
+                my_moves += 1
 
+        opponent_pieces = board.get_all_playable_pieces(opponent_color)
+        opponent_moves = 0
+        for x, y in opponent_pieces:
+            moves = board.get_legal_moves((x, y))
+            for move in moves:
+                opponent_moves += 1
+
+        score += (my_moves - opponent_moves) * 2
+        
         return score
     
-    def choose_move(self, color, board):
+    def minMax(self, color, board, deep, IS_MAX = True):
 
-        all_playable_pieces = [
-            (x, y)
-            for x in range(8)
-            for y in range(8)
-            if board.grid[x][y].piece
-            and board.grid[x][y].piece.color == color
-            and board.get_legal_moves([x, y])
-        ]
+        if deep == 0 or board.check_game_status(color) != 'ongoing':
+            return self.evaluate(color, board)
+        
+        if IS_MAX:
+            current_color = color
+            best_score = -float('inf')
+        else:
+            current_color = 'b' if color == 'w' else 'w'
+            best_score = float('inf')
 
-        best_move = None
-        current_best_score = -float('inf')
+        all_playable_pieces = board.get_all_playable_pieces(current_color)
+
+        if IS_MAX:
+            for x, y in all_playable_pieces:
+                piece = board.grid[x][y].piece
+                moves = board.get_legal_moves((x, y))
+                for a, b in moves:
+                    new_board = copy.deepcopy(board)
+                    new_piece = new_board.grid[x][y].piece
+                    new_board.change_piece(a, b, new_piece, (x, y))
+                    score = self.minMax(color, new_board, deep - 1, not IS_MAX)
+                    best_score = max(best_score, score)
+        else:
+            for x, y in all_playable_pieces:
+                piece = board.grid[x][y].piece
+                moves = board.get_legal_moves((x, y))
+                for a, b in moves:
+                    new_board = copy.deepcopy(board)
+                    new_piece = new_board.grid[x][y].piece
+                    new_board.change_piece(a, b, new_piece, (x, y))
+                    score = self.minMax(color, new_board, deep - 1, not IS_MAX)
+                    best_score = min(best_score, score)
+        
+        return best_score
+        
+
+
+    def choose_move(self, color, board, deep = 2):
+        all_playable_pieces = board.get_all_playable_pieces(color)
+        best_score = -float('inf')
 
         for x, y in all_playable_pieces:
             piece = board.grid[x][y].piece
-            if piece:
-                legal_moves = board.get_legal_moves([x, y])
-                if legal_moves:
-                    for a, b in legal_moves:
-                        captured_piece = board.change_piece(a, b, piece, (x, y))
+            moves = board.get_legal_moves((x, y))
+            for a, b in moves:
+                new_board = copy.deepcopy(board)
+                new_piece = new_board.grid[x][y].piece
+                new_board.change_piece(a, b, new_piece, (x, y))
+                current_score = self.minMax(color, new_board, deep - 1, False)
+                if current_score > best_score:
+                    best_score = current_score
+                    start_best_move_position = (x, y)
+                    end_best_move_position = (a, b)
+
+        return start_best_move_position, end_best_move_position
+        
+
+
+
+
+
+        # all_playable_pieces = [
+        #     (x, y)
+        #     for x in range(8)
+        #     for y in range(8)
+        #     if board.grid[x][y].piece
+        #     and board.grid[x][y].piece.color == color
+        #     and board.get_legal_moves([x, y])
+        # ]
+
+        # best_move = None
+        # current_best_score = -float('inf')
+
+        # for x, y in all_playable_pieces:
+        #     piece = board.grid[x][y].piece
+        #     if piece:
+        #         legal_moves = board.get_legal_moves([x, y])
+        #         if legal_moves:
+        #             for a, b in legal_moves:
+        #                 captured_piece = board.change_piece(a, b, piece, (x, y))
                         
-                        score = self.evaluate(color, board)
+        #                 score = self.evaluate(color, board)
 
-                        board.undo_move((x, y), (a, b), captured_piece)
+        #                 board.undo_move((x, y), (a, b), captured_piece)
 
-                        if score > current_best_score:
-                            current_best_score = score
-                            best_move = [[x, y, a, b]]
-                        elif score == current_best_score:
-                            best_move.append([x, y, a, b])
+        #                 if score > current_best_score:
+        #                     current_best_score = score
+        #                     best_move = [[x, y, a, b]]
+        #                 elif score == current_best_score:
+        #                     best_move.append([x, y, a, b])
                             
         
-        if best_move:
-            random_best_move = random.choice(best_move)
+        # if best_move:
+        #     random_best_move = random.choice(best_move)
 
-            start_position = (random_best_move[0], random_best_move[1])
-            end_position = (random_best_move[2], random_best_move[3])
+        #     start_position = (random_best_move[0], random_best_move[1])
+        #     end_position = (random_best_move[2], random_best_move[3])
 
-            return start_position, end_position
+        #     return start_position, end_position
 
 
-        if not best_move:
-            random_ai = Random_ai()
-            return random_ai.choose_move(color, board)
+        # if not best_move:
+        #     random_ai = Random_ai()
+        #     return random_ai.choose_move(color, board)
 
-        start_position = (best_move[0], best_move[1])
-        end_position = (best_move[2], best_move[3])
+        # start_position = (best_move[0], best_move[1])
+        # end_position = (best_move[2], best_move[3])
 
-        return start_position, end_position
+        # return start_position, end_position
